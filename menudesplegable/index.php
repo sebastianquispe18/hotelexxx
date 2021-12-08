8beb3553e7d56b189a53353f5632a047b12d3b3d
sebastianquispe18 <sebastianquispe044@gmail.com>
2021-12-08 17:47:47 -0300

    Otra pagina

---
+++ menudesplegable/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Menù | Html-Css</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+   <header class="header">
+       <div class="container">
+           <div class="logo">
+               <h1>HOTELEX</h1>
+           </div>
+           <nav class="menu">
+               <ul>
+                   <li><a href="#">Inicio</a></li>
+                   <li><a href="#">Empleados <i class="icon-abajo2"></i></a>
+                   <ul class="submenu">
+                       <li><a href="#">Sebastian</a></li>
+                       <li><a href="#">Ismael</a></li>
+                       <li><a href="#">craks</a></li>
+                   </ul>
+
+                   </li>
+
+                   <li><a href="#">Servicios</a></li>
+                   <li><a href="#">Contacto <i class="icon-abajo2"></i></a>
+                   <ul class="submenu">
+                       <li><a href="#">
+                           Email
+                       </a></li>
+                       <li><a href="#">
+                           Mapa
+                       </a></li>
+                   </ul>
+                   </li>
+                   
+               </ul>
+           </nav>  
+       </div>
+       
+   </header>
+   <section class="port">
+    <div class="portada">
+        <img src="HOTELEXlogo1.jpg">Una empresa de hoteles...
+    </div>
+    </section>
+
+    <section class="contenido">
+    <p>EMPRESA DE HOTELES</p>
+    </section>
+    
+    <section class="contenido2">
+        hola
+    </section>
+
+
+    
+
+    
+
+
+
+    
+
+
+
+    
+
+
+</body>
+</html>
